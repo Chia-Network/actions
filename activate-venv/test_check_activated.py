162c684e7a171cc366bfc662fc13d5a0c775e468
Kyle Altendorf <sda@fstab.net>
2022-11-21 16:13:45 -0500

    rework test_check_activated.py

--- activate-venv/test_check_activated.py
+++ activate-venv/test_check_activated.py
@@ -2,30 +2,25 @@ import os
 import pathlib
 import sys
 
+reference = pathlib.Path(os.environ["REFERENCE_PATH"]).absolute()
 actual = pathlib.Path(sys.executable)
+expected_inside = {"true": True, "false": False}[os.environ["REFERENCE_PATH"]]
 
-automatic_venv = os.environ.get("AUTOMATIC_VENV", "false")
-reference: pathlib.Path
-correct: bool
-if automatic_venv == "false":
-    reference = pathlib.Path.cwd().joinpath(os.environ["EXPECTED_EXECUTABLE"])
-    correct = reference == actual
-elif automatic_venv == "true":
-    reference = pathlib.Path(os.environ["RUNNER_TEMP"])
-    try:
-        actual.relative_to(reference)
-    except ValueError:
-        correct = False
-    else:
-        correct = True
+is_inside: bool
+try:
+    actual.relative_to(reference)
+except ValueError:
+    is_inside = False
 else:
-    print(f"unknown AUTOMATIC_VENV value: {automatic_venv!r}")
-    sys.exit(1)
+    is_inside = True
 
+correct = expected_inside == is_inside
 
-print(f"reference: {reference}")
-print(f"   actual: {actual}")
-print(f"  correct: {'yes' if correct else 'no'}")
+print(f"      reference: {reference}")
+print(f"         actual: {actual}")
+print(f"expected_inside: {expected_inside}")
+print(f"      is_inside: {is_inside}")
+print(f"        correct: {'yes' if correct else 'no'}")
 
 if not correct:
     sys.exit(1)
