cb0d7d0ad408f37c51a5525bf94d7bdb984f5f44
Kyle Altendorf <sda@fstab.net>
2022-11-23 14:02:08 -0500

    add and use python_executable output

--- create-venv/create_temporary_venv.py
+++ create-venv/create_temporary_venv.py
@@ -1,11 +1,21 @@
 import os
 import pathlib
+import sys
 import tempfile
 import venv
 
 runner_temp = os.environ["RUNNER_TEMP"]
 temporary_directory = pathlib.Path(tempfile.mkdtemp(dir=runner_temp))
 github_output = pathlib.Path(os.environ["GITHUB_OUTPUT"])
+
+python_executable: pathlib.Path
+if sys.platform == "win32":
+    python_executable = temporary_directory.joinpath("Scripts", "python.exe")
+else:
+    python_executable = temporary_directory.joinpath("bin", "python")
+
 with github_output.open("a") as file:
-    print(f"temp-venv-path={temporary_directory}", file=file)
+    print(f"temp-venv-path={os.fspath(temporary_directory)}", file=file)
+    print(f"temp-python-executable={os.fspath(python_executable)}", file=file)
+
 venv.create(env_dir=temporary_directory, with_pip=True)
