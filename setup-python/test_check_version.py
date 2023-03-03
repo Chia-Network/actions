da9d358d54cf7b8135537bac6724785ad0c972ef
Kyle Altendorf <sda@fstab.net>
2023-03-03 15:50:47 -0500

    more

--- setup-python/test_check_version.py
+++ setup-python/test_check_version.py
@@ -4,7 +4,7 @@ import sys
 check = os.environ["PYTHON_CHECK"]
 segments = check.split(".")
 
-version = ".".join(str(segment) for segment in sys.version_info[:len(segments)])
+version = ".".join(str(segment) for segment in sys.version_info[: len(segments)])
 
 if version != check:
     sys.exit(1)
