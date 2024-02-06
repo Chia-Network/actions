741a1ff62aa5990fa8bf158cb8f2f0d93f4aeec6
Kyle Altendorf <sda@fstab.net>
2024-02-06 11:56:58 -0500

    diagnostics

--- setup-python/test_check_version.py
+++ setup-python/test_check_version.py
@@ -6,5 +6,8 @@ segments = check.split(".")
 
 version = ".".join(str(segment) for segment in sys.version_info[: len(segments)])
 
+print(f"  actual version: {version!r}")
+print(f"expected version: {check!r}")
+
 if version != check:
     sys.exit(1)
