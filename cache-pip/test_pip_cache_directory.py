393eef4ccdfa73e6a630a983489dfc9f8643e848
Kyle Altendorf <sda@fstab.net>
2024-03-21 08:14:24 -0400

    add test for `cache-pip`

---
+++ cache-pip/test_pip_cache_directory.py
@@ -0,0 +1,23 @@
+import os
+import pathlib
+import subprocess
+
+
+def main() -> None:
+    reference_path = pathlib.Path(os.environ["REFERENCE_PATH"])
+    completed_process = subprocess.run(
+        args=["pip", "cache", "dir"],
+        check=True,
+        stdout=subprocess.PIPE,
+        encoding="utf-8",
+    )
+    pip_cache_path = pathlib.Path(completed_process.stdout)
+
+    assert reference_path.is_absolute(), f"reference path is not absolute: {reference_path}"
+    assert pip_cache_path.is_absolute(), f"pip cache path is not absolute: {pip_cache_path}"
+    assert pip_cache_path.exists(), f"pip cache path does not exist: {pip_cache_path}"
+    assert pip_cache_path.is_dir(), f"pip cache path exists but is not a directory: {pip_cache_path}"
+    pip_cache_path.relative_to(reference_path)
+
+
+main()
