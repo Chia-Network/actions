9ac79da298ad8160868a121f522c942bb1199ca1
Kyle Altendorf <sda@fstab.net>
2024-03-21 13:10:23 -0400

    consistent path

--- cache-pip/cache_pip.py
+++ cache-pip/cache_pip.py
@@ -4,16 +4,21 @@ import tempfile
 
 
 def main() -> None:
-    runner_temp = os.environ["RUNNER_TEMP"]
-    temporary_directory = tempfile.mkdtemp(dir=runner_temp)
+    directory = pathlib.Path(
+        os.environ["RUNNER_TEMP"],
+        ".chia-network-actions-cache-pip",
+    )
+    directory.mkdir(parents=True, exist_ok=False)
+
+    print(f"Cache directory: {directory}")
 
     github_output = pathlib.Path(os.environ["GITHUB_OUTPUT"])
     with github_output.open("a") as file:
-        print(f"dir={temporary_directory}", file=file)
+        print(f"dir={directory}", file=file)
 
     github_env = pathlib.Path(os.environ["GITHUB_ENV"])
     with github_env.open("a") as file:
-        print(f"PIP_CACHE_DIR={temporary_directory}", file=file)
+        print(f"PIP_CACHE_DIR={directory}", file=file)
 
 
 main()
