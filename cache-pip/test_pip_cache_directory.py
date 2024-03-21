5eabcd182dd31a3039e5bd9fecf30e9b82cffaff
Kyle Altendorf <sda@fstab.net>
2024-03-21 08:46:21 -0400

    tidy

--- cache-pip/test_pip_cache_directory.py
+++ cache-pip/test_pip_cache_directory.py
@@ -4,7 +4,9 @@ import subprocess
 
 
 def main() -> None:
-    reference_path = pathlib.Path(os.environ["REFERENCE_PATH"])
+    # macOS mktemp does not support specification of path base so using regular tmp
+    # directory for now.
+    # reference_path = pathlib.Path(os.environ["RUNNER_TEMP"])
     completed_process = subprocess.run(
         args=["pip", "cache", "dir"],
         check=True,
@@ -13,12 +15,10 @@ def main() -> None:
     )
     pip_cache_path = pathlib.Path(completed_process.stdout.strip())
 
-    assert reference_path.is_absolute(), f"reference path is not absolute: {reference_path!r}"
+    # assert reference_path.is_absolute(), f"reference path is not absolute: {reference_path!r}"
     assert pip_cache_path.is_absolute(), f"pip cache path is not absolute: {pip_cache_path!r}"
     assert pip_cache_path.exists(), f"pip cache path does not exist: {pip_cache_path!r}"
     assert pip_cache_path.is_dir(), f"pip cache path exists but is not a directory: {pip_cache_path!r}"
-    # macOS mktemp does not support specification of path base so using regular tmp
-    # directory for now.
     # pip_cache_path.relative_to(reference_path)
 
 
