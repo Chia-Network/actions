f5951b05894e17337c27be4f4c671ed723ac5422
Chris Marslender <chrismarslender@gmail.com>
2024-05-07 16:36:33 -0500

    Disable JSCPD (#165)
    * Disable JSCPD
    
    * Fix shfmt
    
    * Turn off pylint for now
    
    * No checkov for now
    
    * Fix shfmt again

--- update-dependabot-config.sh
+++ update-dependabot-config.sh
@@ -6,11 +6,10 @@ file=.github/dependabot.yml
 
 yq eval -i 'del(.updates[] | select(. | headComment == "generated"))' "${file}"
 
-for action in $(git ls-files | sed -n 's;/action.yml$;;p')
-do
-  export action="${action}"
-  echo processing: "${action}"
-  yq eval -i '.updates += (.updates | filter(.directory == "/") | with(.[]; .directory = env(action))) | .updates.[-1] head_comment= "generated"' "${file}"
+for action in $(git ls-files | sed -n 's;/action.yml$;;p'); do
+	export action="${action}"
+	echo processing: "${action}"
+	yq eval -i '.updates += (.updates | filter(.directory == "/") | with(.[]; .directory = env(action))) | .updates.[-1] head_comment= "generated"' "${file}"
 done
 
 git diff --exit-code --quiet "${file}"
