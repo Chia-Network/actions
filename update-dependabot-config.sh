81bc98b09376cc632e228f090563e2965e368355
Chris Marslender <chrismarslender@gmail.com>
2024-05-07 16:12:19 -0500

    Fix shfmt

--- update-dependabot-config.sh
+++ update-dependabot-config.sh
@@ -6,8 +6,7 @@ file=.github/dependabot.yml
 
 yq eval -i 'del(.updates[] | select(. | headComment == "generated"))' "${file}"
 
-for action in $(git ls-files | sed -n 's;/action.yml$;;p')
-do
+for action in $(git ls-files | sed -n 's;/action.yml$;;p'); do
   export action="${action}"
   echo processing: "${action}"
   yq eval -i '.updates += (.updates | filter(.directory == "/") | with(.[]; .directory = env(action))) | .updates.[-1] head_comment= "generated"' "${file}"
