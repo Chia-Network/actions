62bab199d915c3aac691f89c524405f31d1da65c
Kyle Altendorf <sda@fstab.net>
2024-08-12 12:52:30 -0400

    some testing

--- cache-pip/test_pip_cache_directory.py
+++ cache-pip/test_pip_cache_directory.py
@@ -1,29 +1,44 @@
+import enum
 import os
 import pathlib
 import subprocess
+import sys
+from typing import Sequence
 
 
-def main() -> None:
+class Mode(enum.Enum):
+    setuptools = "setuptools"
+    poetry = "poetry"
+
+
+def main(args: Sequence[str]) -> None:
+    mode = Mode[args[0]]
+
     reference_path = pathlib.Path(os.environ["RUNNER_TEMP"])
-    completed_process = subprocess.run(
-        args=["pip", "cache", "dir"],
-        check=True,
-        stdout=subprocess.PIPE,
-        encoding="utf-8",
-    )
-    pip_cache_path = pathlib.Path(completed_process.stdout.strip())
+
+    if mode == Mode.setuptools:
+        completed_process = subprocess.run(
+            args=["pip", "cache", "dir"],
+            check=True,
+            stdout=subprocess.PIPE,
+            encoding="utf-8",
+        )
+        cache_path = pathlib.Path(completed_process.stdout.strip())
+    elif mode == Mode.poetry:
+        # TODO: implement a poetry command to report the cache directories
+        cache_path = os.environ["POETRY_CACHE_DIR"]
 
     assert (
         reference_path.is_absolute()
     ), f"reference path is not absolute: {reference_path!r}"
     assert (
-        pip_cache_path.is_absolute()
-    ), f"pip cache path is not absolute: {pip_cache_path!r}"
-    assert pip_cache_path.exists(), f"pip cache path does not exist: {pip_cache_path!r}"
+        cache_path.is_absolute()
+    ), f"cache path is not absolute: {cache_path!r}"
+    assert cache_path.exists(), f"cache path does not exist: {cache_path!r}"
     assert (
-        pip_cache_path.is_dir()
-    ), f"pip cache path exists but is not a directory: {pip_cache_path!r}"
-    pip_cache_path.relative_to(reference_path)
+        cache_path.is_dir()
+    ), f"cache path exists but is not a directory: {cache_path!r}"
+    cache_path.relative_to(reference_path)
 
 
-main()
+main(sys.argv[1:])
