5b7100232c8bdbc87210dea08e38f1c3f2fe3ef4
Matt <quexington@gmail.com>
2025-10-07 09:57:27 -0700

    Some cleanup

--- setup-python/test_check_version.py
+++ setup-python/test_check_version.py
@@ -1,14 +1,13 @@
-# import os
-# import sys
+import os
+import sys
 
-# check = os.environ["PYTHON_CHECK"]
-# segments = check.split(".")
+check = os.environ["PYTHON_CHECK"]
+segments = check.split(".")
 
-# version = ".".join(str(segment) for segment in sys.version_info[: len(segments)])
+version = ".".join(str(segment) for segment in sys.version_info[: len(segments)])
 
-# print(f"  actual version: {version!r}")
-# print(f"expected version: {check!r}")
+print(f"  actual version: {version!r}")
+print(f"expected version: {check!r}")
 
-# if version != check:
-#     sys.exit(1)
-pass
+if version != check:
+    sys.exit(1)
--- setup-python/test_check_version.sh
+++ setup-python/test_check_version.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 set -o errexit
 
-echo "Path is $PATH"
-echo "Github Path is $GITHUB_PATH"
-
 $PYTHON_SHIM --version
